be67454397da95736c6956dc3f7e1206047da867
AlexGuoussatchok <115575196+AlexGuoussatchok@users.noreply.github.com>
2022-10-17 17:08:55 +0300

    General_task_2 added

--- General_task_01.py
+++ General_task_01.py
@@ -2,7 +2,42 @@
 # QA4822
 # Stage 09
 # Task: Compare digits in a number
-# V 1.0 (using 'while')
+# V 1.0 (using 'while') May be not the exact solution to a task, cause probably the task was to make the programme
+# tell "Yes" or "No" to the question if all of the digits are equal
+
+# def user_input():
+#     num = int(input("Please, enter a number: "))
+#     num *= -1 if num < 0 else 1  # if a number < 0 entered - we still will be able to compare digits
+#     return num
+#
+#
+# def digits_compare(num):
+#     result = "equal"  # value by default
+#     while num // 10 > 0:
+#         if num % 10 != num // 10 % 10:  # if compared digits are not equal --> default value will be changed
+#             result = "not equal"
+#             break  # --> if one couple of compared digits is not equal - there's no need in further comparison
+#         else:
+#             num //= 10
+#     return result
+#
+#
+# def main():
+#     num = user_input()
+#     if 0 >= num // 10 < 1:
+#         print(f"The number you've entered ({num}) consists of 1 digit, so there is nothing to compare. "
+#               f"Please enter a number of 2 digits minimum.")  # if a number with only one digit is entered -
+#         # there's nothing to compare
+#     else:
+#         result = digits_compare(num)
+#         msg = f"Digits in number you've entered ({num}) are {result}"
+#         print(msg)
+#
+#
+# main()
+
+#--------------------------------------------------------------------------------------------------------------------
+# v 1.1 - If all of number digits are equal - the programme will return "Yes", if not equal - will return "No"
 
 def user_input():
     num = int(input("Please, enter a number: "))
@@ -11,11 +46,11 @@ def user_input():
 
 
 def digits_compare(num):
-    result = "equal"  # value by default
+    result = "Yes"  # value by default
     while num // 10 > 0:
         if num % 10 != num // 10 % 10:  # if compared digits are not equal --> default value will be changed
-            result = "not equal"
-            break  # --> if one couple of compared digits is not equal - there's no need in further compare
+            result = "No"
+            break  # --> if one couple of compared digits is not equal - there's no need in further comparison
         else:
             num //= 10
     return result
@@ -29,7 +64,7 @@ def main():
         # there's nothing to compare
     else:
         result = digits_compare(num)
-        msg = f"Digits in number you've entered ({num}) are {result}"
+        msg = f"Are all number digits in (\"{num}\") are equal? - {result}"
         print(msg)
 
 
